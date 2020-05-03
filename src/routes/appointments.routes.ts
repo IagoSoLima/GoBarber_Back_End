676a54590a12d7e49b14272d8566a6626a6b8ba4
Iago de Souza Lima <iago.souza069@gmail.com>
2020-05-03 20:10:12 -0300

    chore: inicialization project

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
+
+import AppointmentRepository from '../repositories/AppointmentRepository';
+import CreateAppointmentService from '../service/CreateAppointmentService';
+
+const appointmentRouter = Router();
+const appointmentRepository = new AppointmentRepository();
+
+appointmentRouter.get('/', (request, response) => {
+  const appointment = appointmentRepository.all();
+
+  return response.json(appointment);
+});
+
+appointmentRouter.post('/', (request, response) => {
+  try {
+    const { provider, date } = request.body;
+    const parsedDate = parseISO(date);
+
+    const createAppointment = new CreateAppointmentService(
+      appointmentRepository,
+    );
+
+    const appointment = createAppointment.execute({
+      date: parsedDate,
+      provider,
+    });
+
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ message: err.message });
+  }
+});
+
+export default appointmentRouter;
