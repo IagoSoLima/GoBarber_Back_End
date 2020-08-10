bc9eb62537f484eb603f0ffadf9b963b9763da11
Iago de Souza Lima <iago.souza069@gmail.com>
2020-08-09 21:02:08 -0300

    feature: create a MailProvider with EtherealMailProvider

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -9,8 +9,8 @@ import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepo
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ApponintmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentRepository';
 
-// import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
-// import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+import UsersTokensRepository from '@modules/users/infra/typeorm/repositories/UsersTokensRepository';
 
 container.registerSingleton<IAppointmentsRepository>(
   'ApponintmentsRepository',
@@ -22,7 +22,7 @@ container.registerSingleton<IUsersRepository>(
   UsersRepository,
 );
 
-// container.registerSingleton<IUserTokensRepository>(
-//   'UserTokensRepository',
-//   UserTokensRepository,
-// );
+container.registerSingleton<IUserTokensRepository>(
+  'UserTokensRepository',
+  UsersTokensRepository,
+);
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,10 +2,13 @@ import { Router } from 'express';
 import appointmentRouter from '@modules/appointments/infra/http/routes/appointments.routes';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
+import PasswordRouter from '@modules/users/infra/http/routes/password.routes';
 
 const routes = Router();
 routes.use('/appointments', appointmentRouter);
 routes.use('/users', usersRouter);
+routes.use('/sessions', PasswordRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/password', PasswordRouter);
 
 export default routes;
