d24dee6c3d3ea3b07b4186610b374c7eb745220c
Iago de Souza Lima <iago.souza069@gmail.com>
2020-08-09 23:10:20 -0300

    feature: add template email engine for send emails

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,13 +3,20 @@ import DiskStorageProvider from './StorageProvider/implementations/DiskStoragePr
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
 import IMailProvider from './MailProvider/models/IMailProvider';
+import IMailTemplateProvider from './MailTempalateProvider/models/IMailTemplateProvider';
+import HandlebarsMailTemplateProvider from './MailTempalateProvider/implamentations/HandlebarsmailTemplateProvider';
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
 );
 
+container.registerSingleton<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  HandlebarsMailTemplateProvider,
+);
+
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  new EtherealMailProvider(),
+  container.resolve(EtherealMailProvider),
 );
